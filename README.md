2b3d936bfce533e4dee78c7449fac4c1a00eee86
laopo001 <0laopo0@gmail.com>
2023-09-27 17:22:05 +0800

    add cf page preview

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bevy-tank-game
 Tank game developed by bevy engine.
+
+# DEMO
+
+[demo](https://bevy-tank-game.pages.dev/)
--- wasm.mjs
+++ wasm.mjs
@@ -1,3 +1,5 @@
+#!/usr/bin/env zx
+
 argv.debug = argv.debug == null ? 1 : +argv.debug;
 console.log('debug: ', argv.debug)
 if (argv.debug == 1) {
@@ -5,11 +7,18 @@ if (argv.debug == 1) {
     await $`wasm-bindgen target/wasm32-unknown-unknown/debug/bevy-tank-game.wasm --out-dir wasm --web --out-name index`.catch(e => {
         console.log("请按照wasm-bindgen"); throw e;
     })
+    await $`http-server .`
 } else {
     await $`cargo build --target wasm32-unknown-unknown --release`
     await $`wasm-bindgen target/wasm32-unknown-unknown/release/bevy-tank-game.wasm --out-dir wasm --web --out-name index`.catch(e => {
-        console.log("请按照wasm-bindgen"); throw e;
+        console.log("请安装wasm-bindgen"); throw e;
     })
+    await $`wasm-opt -Os -o wasm/index_bg.wasm wasm/index_bg.wasm`.catch(e => {
+        console.log("请安装wasm-opt"); throw e;
+    })
+    await fs.emptyDir("web")
+    await fs.copy("wasm", "web/wasm")
+    await fs.copy("assets", "web/assets")
+    await fs.copy("index.html", "web/index.html")
+    await $(`wrangler pages deploy web --project-name=bevy-tank-game --branch=main`)
 }
-
-await $`http-server .`
