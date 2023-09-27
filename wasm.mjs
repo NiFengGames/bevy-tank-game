e8781c8b0d1b928dbff03692537b44b9abfc32ed
laopo001 <0laopo0@gmail.com>
2023-09-27 17:41:51 +0800

    fix

--- wasm.mjs
+++ wasm.mjs
@@ -20,5 +20,5 @@ if (argv.debug == 1) {
     await fs.copy("wasm", "web/wasm")
     await fs.copy("assets", "web/assets")
     await fs.copy("index.html", "web/index.html")
-    await $(`wrangler pages deploy web --project-name=bevy-tank-game --branch=main`)
+    await $`wrangler pages deploy web --project-name=bevy-tank-game --branch=main`
 }
